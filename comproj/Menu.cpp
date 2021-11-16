7058f631a5c563d5b1c3ee014c48944bdd017f16
Butyrev Nikita <bggniklol@gmail.com>
2021-11-16 13:37:49 +0300

    добавлены методы ввода новых данных, редактирования существующих

--- comproj/Menu.cpp
+++ comproj/Menu.cpp
@@ -64,3 +64,221 @@ void Menu::Update(){	//обновление меню
 		_getch();//читаем символ, что бы не переходило к следуюшей итерации
 	}
 }
+
+void Menu::InputRecord(){	//ввод записи
+	int sid; //ид
+	int sday;	//день
+	int smonth;	//месяц
+	int syear;	//год
+	int shour;	//час
+	int smin;	//минуты
+	string slname;	//фамилия
+	string sfname;	//имя
+	string spatron;	//отчество
+	long long snumber;	//номер
+	string smaster;	//имя мастера
+	string stype;	//тип
+	int sprice;	//стоимость
+	do{
+		cout << "\n\nВведите id записи:";
+		enterInt(&sid);//функция ввода целого числа
+		if (sid > 900)
+			cout << "Слишком большое id" << endl;
+		if (ptrRecordList->FindId(sid) != NULL)
+			cout << "Запись с таким id уже существует, повторите ввод." << endl;
+	} while ((!checkNegative(sid)) || (sid > 900) || (ptrRecordList->FindId(sid) != NULL)); //условие выполнения, не отриц, 
+	//не больше 900, нет такого ид
+	do{
+		cout << "Введите день:";
+		enterInt(&sday);
+	} while (!checkDay(sday));
+	////////////////////////
+	do{
+		cout << "Введите месяц:";
+		enterInt(&smonth);
+	} while (!checkMonth(smonth));
+	////////////////////////
+	cout << "Введите год:";
+	do{
+		enterInt(&syear);
+	} while (!checkYear(syear));
+	////////////////////////
+	do{
+		cout << "Введите часы:";
+		enterInt(&shour);
+	} while (!checkHour(shour));
+	////////////////////////
+
+	do{
+		cout << "Введите минуты:";
+		enterInt(&smin);
+	} while (!checkMin(smin));
+	////////////////////////
+	do{
+		cout << "Введите Имя:";
+		sfname.clear();
+		getline(cin, sfname);
+		if (sfname.size() > 12)
+			cout << "Слишком длинная строка" << endl;
+	} while (sfname.size() > 12);
+	////////////////////////
+	do{
+		cout << "Введите Фамилию:";
+		slname.clear();
+		getline(cin, slname);
+		if (slname.size() > 12)
+			cout << "Слишком длинная строка" << endl;
+	} while (slname.size() > 12);
+	////////////////////////
+	do{
+		cout << "Введите Отчество:";
+		spatron.clear();
+		getline(cin, spatron);
+		if (spatron.size() > 12)
+			cout << "Слишком длинная строка" << endl;
+	} while (spatron.size() > 12);
+	////////////////////////
+	do{
+		cout << "Введите номер телефона(В формате 89000000000):";
+		enterLongLong(&snumber);
+	} while (!checkNumber(snumber));
+	////////////////////////
+	do{
+		cout << "Введите имя мастера:";
+		smaster.clear();
+		getline(cin, smaster);
+		if (smaster.size() > 12)
+			cout << "Слишком длинная строка" << endl;
+	} while (smaster.size() > 12);
+	////////////////////////
+	do{
+		cout << "Введите вид услуги:";
+		stype.clear();
+		getline(cin, stype);
+		if (stype.size() > 15)
+			cout << "Слишком длинная строка" << endl;
+	} while (stype.size() > 15);
+	////////////////////////
+	do{
+		cout << "Введите стоимость:";
+		enterInt(&sprice);
+	} while (!checkNegative(sprice));
+	Record *ptrNewRec = new Record(sid, sday, smonth, syear, shour, smin, sfname, slname, spatron, snumber, smaster, stype, sprice);
+	ptrRecordList->InsertRecord(ptrNewRec);
+}
+
+void Menu::EditRecord(){
+	int sid;
+	int sday;
+	int smonth;
+	int syear;
+	int shour;
+	int smin;
+	string slname;
+	string sfname;
+	string spatron;
+	long long snumber;
+	string smaster;
+	string stype;
+	int sprice;
+	Record* eRec;
+	do{
+		cout << "\n\nВведите id записи для редактирования:";
+		enterInt(&sid);
+	} while (!checkNegative(sid));
+	if (eRec = ptrRecordList->FindId(sid)){
+		printHeader();
+		printFooter();
+		eRec->Print();
+		printFooter();
+		cout << endl;
+		do{
+			cout << "Введите день:";
+			enterInt(&sday);
+		} while (!checkDay(sday));
+		eRec->SetDay(sday);
+		////////////////////////
+		do{
+			cout << "Введите месяц:";
+			enterInt(&smonth);
+		} while (!checkMonth(smonth));
+		eRec->SetMonth(smonth);
+		////////////////////////
+		do{
+			cout << "Введите год(В формате 0000):";
+			enterInt(&syear);
+		} while (!checkYear(syear));
+		eRec->SetYear(syear);
+		////////////////////////
+		do{
+			cout << "Введите часы:";
+			enterInt(&shour);
+		} while (!checkHour(shour));
+		eRec->SetHour(shour);
+		////////////////////////
+		do{
+			cout << "Введите минуты:";
+			enterInt(&smin);
+		} while (!checkMin(smin));
+		eRec->SetMin(smin);
+		////////////////////////
+		do{
+			cout << "Введите Имя:";
+			sfname.clear();
+			getline(cin, sfname);
+			if (sfname.size() > 12)
+				cout << "Слишком длинная строка" << endl;
+		} while (sfname.size() > 12);
+		eRec->SetFname(sfname);
+		////////////////////////
+		do{
+			cout << "Введите Фамилию:";
+			slname.clear();
+			getline(cin, slname);
+			if (slname.size() > 12)
+				cout << "Слишком длинная строка" << endl;
+		} while (slname.size() > 12);
+		eRec->SetLname(slname);
+		////////////////////////
+		do{
+			cout << "Введите Отчество:";
+			spatron.clear();
+			getline(cin, spatron);
+			if (spatron.size() > 12)
+				cout << "Слишком длинная строка" << endl;
+		} while (spatron.size() > 12);
+		eRec->SetPatron(spatron);
+		////////////////////////
+		do{
+			cout << "Введите номер телефона(В формате 89000000000):";
+			enterLongLong(&snumber);
+		} while (!checkNumber(snumber));
+		eRec->SetNumber(snumber);
+		////////////////////////
+		do{
+			cout << "Введите имя мастера:";
+			smaster.clear();
+			getline(cin, smaster);
+			if (smaster.size() > 12)
+				cout << "Слишком длинная строка" << endl;
+		} while (smaster.size() > 12);
+		eRec->SetMaster(smaster);
+		////////////////////////
+		do{
+			cout << "Введите вид услуги:";
+			stype.clear();
+			getline(cin, stype);
+			if (stype.size() > 15)
+				cout << "Слишком длинная строка" << endl;
+		} while (stype.size() > 15);
+		eRec->SetType(stype);
+		////////////////////////
+		do{
+			cout << "Введите стоимость:";
+			enterInt(&sprice);
+		} while (!checkNegative(sprice));
+		eRec->SetPrice(sprice);
+	}
+	else
+		cout << "Запись не найдена" << endl;
+}
